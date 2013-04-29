ef3b40c5563c8ed1adf43e038e035333b1cfcd48
Edward Soo <paktiksoo@gmail.com>
2013-04-28 17:22:40 -0700

    added facebook php sdk

--- index.php
+++ index.php
@@ -1,40 +1,71 @@
-<html><body>
-
 <?php
-  $app_id = '519404198096257';
-  $app_secret = '586e0fb7210204eb364143c2cf6de381';
-  $my_url = 'http://secret-ocean-1139.herokuapp.com';
+// Modified version of facebook php sdk example
 
-  $code = $_REQUEST["code"];
+require 'facebook-php-sdk/src/facebook.php';
+$config = array(
+  'appId'  => '519404198096257',
+  'secret' => '586e0fb7210204eb364143c2cf6de381',
+  );
 
- // auth user
- if(empty($code)) {
-    $dialog_url = 'https://www.facebook.com/dialog/oauth?client_id=' 
-    . $app_id . '&redirect_uri=' . urlencode($my_url) . "&scope=read_mailbox";
-    echo("<script>top.location.href='" . $dialog_url . "'</script>");
+$facebook = new Facebook($config);
+$user = $facebook->getUser();
+
+if ($user) {
+  try {
+    // Proceed knowing you have a logged in user who's authenticated.
+    $user_profile = $facebook->api('/me');
+  } catch (FacebookApiException $e) {
+    error_log($e);
+    $user = null;
   }
-  
-  // get user access_token
-  $token_url = 'https://graph.facebook.com/oauth/access_token?client_id='
-    . $app_id . '&redirect_uri=' . urlencode($my_url) 
-    . '&client_secret=' . $app_secret 
-    . '&code=' . $code;
+}
 
-  // response is of the format "access_token=AAAC..."
-  $access_token = substr(file_get_contents($token_url), 13);
-  
-  // run fql query to get messages
-  $fql_query_url = 'https://graph.facebook.com/'
-    . 'fql?q=SELECT+created_time+FROM+message+WHERE+thread_id+IN+(SELECT+thread_id+FROM thread+WHERE+folder_id+IN+(0,1))+AND+author_id=me()'
-    . '&access_token=' . $access_token;
-  $fql_query_result = file_get_contents($fql_query_url);
-  $fql_query_obj = json_decode($fql_query_result, true);
-  
-  // display results of fql query
-  echo '<pre>';
-  print_r("query results:");
-  print_r($fql_query_obj);
-  echo '</pre>';
+// Login or logout url will be needed depending on current user state.
+if ($user) {
+  $logoutUrl = $facebook->getLogoutUrl();
+  $params = array(
+    'method' => 'fql.query',
+    'query' => 'SELECT created_time FROM message WHERE thread_id IN (SELECT thread_id FROM thread WHERE folder_id IN (0,1)) AND author_id=me()');
+  $rs1 = $facebook->api($params);
+  $params = array(
+    'method' => 'fql.query',
+    'query' => 'SELECT created_time FROM message WHERE thread_id IN (SELECT thread_id FROM thread WHERE folder_id IN (0,1)) AND author_id != me()');
+  $rs2 = $facebook->api($params);
+
+
+} else {
+  $loginUrl = $facebook->getLoginUrl(array(
+    "scope" => "read_mailbox"
+    ));
+}
 
 ?>
-</body></html>
+<!doctype html>
+<html xmlns:fb="http://www.facebook.com/2008/fbml">
+<head>
+  <title>PM histogram</title>
+</head>
+<body>
+  <h1>php-sdk</h1>
+
+  <?php if ($user): ?>
+  <a href="<?php echo $logoutUrl; ?>">Logout</a>
+  <?php echo($rs1) ?>
+<?php else: ?>
+  <div>
+    <a href="<?php echo $loginUrl; ?>">Login with Facebook</a>
+  </div>
+<?php endif ?>
+
+<?php if ($user): ?>
+  <h3>You</h3>
+  <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
+
+  <h3>Your User Object (/me)</h3>
+  <pre><?php print_r($user_profile); ?></pre>
+<?php else: ?>
+  <strong><em>You are not Connected.</em></strong>
+<?php endif ?>
+</body>
+</html>
+
